84bab02801866fdd1ff4f81a102b319da50b8c8c
Fernando G <fernanprojects@gmail.com>
2025-01-19 12:15:04 +0100

    remove buildMultiState

--- __tests__/URLStateHandler.test.ts
+++ __tests__/URLStateHandler.test.ts
@@ -1,5 +1,36 @@
+import { URLStateHandler } from "../src";
+
 describe("On URLStateHandler", () => {
-  it("WHEN ..., THEN ...", async () => {
-    console.log("Hello");
+  it("Get State", async () => {
+    const myUrl = "https://www.custom-url.com/path?form=a&form.input=c";
+    const urlSearchParams = new URL(myUrl).search;
+
+    const formUrlStateHandler = URLStateHandler.build({
+      name: "form",
+      values: ["a", "b", "c"],
+      defaultValue: "c",
+    });
+
+    const state = formUrlStateHandler.getState(urlSearchParams);
+
+    expect(state).toEqual("a");
+  });
+
+  it("Set State", async () => {
+    const myUrl = "https://www.custom-url.com/path?form=a&form.input=c";
+    const searchParams = new URL(myUrl).search;
+
+    const formUrlStateHandler = URLStateHandler.build({
+      name: "form",
+      values: ["a", "b", "c"],
+      defaultValue: "c",
+    });
+
+    const state = formUrlStateHandler.setState(searchParams, "b");
+
+    const urlSearchParams = new URLSearchParams(searchParams);
+    urlSearchParams.set("form", "b");
+
+    expect(state).toEqual(urlSearchParams.toString());
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,45 @@
-type URLSearchParamsProps = ConstructorParameters<
-  typeof URLSearchParams
->[number];
-type UrlStateProps<T extends string> = {
+type URLSearchParamsProps = ConstructorParameters<typeof URLSearchParams>[number];
+
+type Props<TValue extends string> = {
   name: string;
-  values: ReadonlyArray<T>;
-  defaultValue: Readonly<T>;
+  values: ReadonlyArray<TValue>;
+  defaultValue: Readonly<TValue>;
+};
+type BuildComposedProps = {
+  key: string;
+  ids: Record<
+    string,
+    {
+      values: string[];
+      defaultValue: string;
+    }
+  >;
 };
 
-class URLStateHandler<TValues extends string, TStates extends string = string> {
-  private constructor(
-    private props: UrlStateProps<TValues>,
-    private mutliState?: ReadonlyArray<TStates>
-  ) {}
+export class URLStateHandler<TValue extends string> {
+  private constructor(private props: Props<TValue>) {
+    this.props = props;
+  }
 
-  static build<TValues extends string>(props: UrlStateProps<TValues>) {
+  static build<TValues extends string>(props: Props<TValues>) {
     return new URLStateHandler(props);
   }
 
-  private static buildMultiState<TV extends string, TS extends string>(
-    props: UrlStateProps<TV>,
-    mutliState: ReadonlyArray<TS>
-  ) {
-    return new URLStateHandler(props, mutliState);
-  }
-
   getState(searchParamsProps: URLSearchParamsProps) {
-    const queryNames = this.getQueryNames();
-    if (typeof queryNames === "string")
-      return this.getStateByName(searchParamsProps, queryNames);
+    const urlSearchParams = new URLSearchParams(searchParamsProps);
 
-    const entries = queryNames.map((name) => {
-      return [name, this.getStateByName(searchParamsProps, name)];
-    });
+    const queryFound = urlSearchParams.get(this.props.name);
 
-    return Object.fromEntries(entries) as Record<TStates, TValues>;
+    if (!queryFound) return this.props.defaultValue;
+
+    const valueFound = this.props.values.find((v) => v === queryFound);
+
+    if (!valueFound) return this.props.defaultValue;
+
+    return valueFound;
   }
 
-  setState(url: URLSearchParamsProps, value?: Readonly<TValues>) {
+  setState(url: URLSearchParamsProps, value?: Readonly<TValue>) {
     const urlSearchParams = new URLSearchParams(url);
 
     if (!value) {
@@ -47,49 +50,4 @@ class URLStateHandler<TValues extends string, TStates extends string = string> {
     urlSearchParams.set(this.props.name, `${value}`);
     return urlSearchParams.toString();
   }
-
-  setMultiState<TS extends string>(s: TS[]): URLStateHandler<TValues, TS> {
-    return URLStateHandler.buildMultiState<TValues, TS>(this.props, s);
-  }
-
-  getQueryNames() {
-    if (!this.mutliState || !this.mutliState.length) {
-      return this.props.name;
-    }
-
-    const states = this.mutliState.map((st) =>
-      this.props.name.concat(`.${st}`)
-    );
-    return [this.props.name, ...states];
-  }
-
-  private getStateByName(
-    searchParamsProps: URLSearchParamsProps,
-    name: string
-  ) {
-    const urlSearchParams = new URLSearchParams(searchParamsProps);
-
-    const queryFound = urlSearchParams.get(name);
-
-    if (!queryFound) return this.props.defaultValue;
-
-    const valueFound = this.props.values.find((v) => v === queryFound);
-
-    if (!valueFound) return this.props.defaultValue;
-
-    return valueFound;
-  }
 }
-
-const myUrl = "https://www.custom-url.com/path?form=a&form.input=c";
-const urlSearchParams = new URL(myUrl).search;
-
-const formUrlStateHandler = URLStateHandler.build({
-  name: "form",
-  values: ["a", "b", "c"],
-  defaultValue: "c",
-});
-
-const multiForm = formUrlStateHandler.setMultiState(["input", "check"]);
-
-console.log(multiForm.getState(urlSearchParams));
