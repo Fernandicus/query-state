ef60def1fbf2fd730a46b2d0f694a4355e295d62
Fernando G <fernanprojects@gmail.com>
2025-01-19 13:20:58 +0100

    Test  buildComposed

--- __tests__/URLStateHandler.test.ts
+++ __tests__/URLStateHandler.test.ts
@@ -33,4 +33,36 @@ describe("On URLStateHandler", () => {
 
     expect(state).toEqual(urlSearchParams.toString());
   });
+
+  it.only("Set State", async () => {
+    const checkValidValues = ["x", "y", "z"];
+    const checkUrlValues = ["x", "y", "o"];
+
+    const myUrl = `https://www.custom-url.com/path?form.inpt=a&form.chks=${checkUrlValues.join(",")}`;
+    const searchParams = new URL(myUrl).search;
+
+    const formUrlStateHandler = URLStateHandler.buildComposed({
+      key: "form",
+      ids: {
+        inpt: {
+          defaultValue: "a",
+          values: ["a", "b", "c"],
+        },
+        chks: {
+          values: checkValidValues,
+          defaultValue: "x",
+        },
+      },
+    });
+
+    const state = formUrlStateHandler.chks.getState(searchParams);
+
+    const expectedResult = () => {
+      const setOfValues = new Set(checkValidValues);
+      const matchedValues = new Set(checkUrlValues).intersection(setOfValues);
+      return Array.from(matchedValues);
+    };
+
+    expect(state).toEqual(expect.arrayContaining(expectedResult()));
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,18 @@ type Props<TValue extends string> = {
   values: ReadonlyArray<TValue>;
   defaultValue: Readonly<TValue>;
 };
-type BuildComposedProps = {
-  key: string;
-  ids: Record<
-    string,
-    {
-      values: string[];
-      defaultValue: string;
-    }
-  >;
+
+type Id<TId extends string, TValue extends string> = Record<
+  TId,
+  {
+    values: ReadonlyArray<TValue>;
+    defaultValue: Readonly<TValue>;
+  }
+>;
+
+type BuildComposedProps<TKey extends string, TId extends string, TValue extends string> = {
+  key: TKey;
+  ids: Id<TId, TValue>;
 };
 
 export class URLStateHandler<TValue extends string> {
@@ -25,12 +28,38 @@ export class URLStateHandler<TValue extends string> {
     return new URLStateHandler(props);
   }
 
+  static buildComposed<TKey extends string, TId extends string>({
+    key,
+    ids,
+  }: BuildComposedProps<TKey, TId, TId[number]>): Record<TId, URLStateHandler<TId[number]>> {
+    const all = Object.keys(ids).map((name) => {
+      return [
+        `${name}` as keyof typeof ids,
+        new URLStateHandler({
+          name: `${key}.${name}`,
+          values: ids[name].values,
+          defaultValue: ids[name].defaultValue,
+        }),
+      ];
+    });
+
+    return Object.fromEntries(all);
+  }
+
   getState(searchParamsProps: URLSearchParamsProps) {
     const urlSearchParams = new URLSearchParams(searchParamsProps);
 
     const queryFound = urlSearchParams.get(this.props.name);
 
     if (!queryFound) return this.props.defaultValue;
+
+    const splitedQueryFound = queryFound.split(",");
+
+    if (splitedQueryFound.length > 1) {
+      const setOfValues = new Set(this.props.values);
+      const matchedValues = new Set(splitedQueryFound).intersection(setOfValues);
+      return Array.from(matchedValues);
+    }
 
     const valueFound = this.props.values.find((v) => v === queryFound);
 
